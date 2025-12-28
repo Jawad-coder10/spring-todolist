d24dc4bfc40988e92c76a3d21eaf508a1a233f35
Jawad-coder10 <djawadisaindou@gmail.com>
2025-12-28 14:53:33 +0100

    Initial Spring Boot TodoList project

---
+++ src/main/java/com/djawadi/springtodolist/SpringTodolistApplication.java
@@ -0,0 +1,12 @@
+package com.djawadi.springtodolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTodolistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTodolistApplication.class, args);
+    }
+}
